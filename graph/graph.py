180c10eefe12e617e7404421328e0299cc56a99d
SomanAbbasi <mssabbasi306@gmail.com>
2026-07-08 23:57:13 +0500

    feat: adding planner node

--- graph/graph.py
+++ graph/graph.py
@@ -1,12 +1,11 @@
 
 
-
-from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-
 from prompts import planner_prompt
 from state import Plan,File
 
+from langchain_groq import ChatGroq
+from langgraph.graph import StateGraph
 
 load_dotenv()
 
@@ -16,10 +15,38 @@ load_dotenv()
 llm=ChatGroq(model="qwen/qwen3.6-27b")
 
 
-user_prompt="Create a Simple Todo List app"
 
+
+def planner_agent(state: dict) -> dict:
+    """Converts user prompt into a structured Plan."""
+    user_prompt = state["user_prompt"]
+    res = llm.with_structured_output(Plan).invoke(
+        planner_prompt(user_prompt)
+    )
+    if res is None:
+        raise ValueError("Planner did not return a valid response.")
+    return {"plan": res}
+
+
+
+    
+user_prompt="Create a Simple Todo List app"
 prompt=planner_prompt(user_prompt)
+
 
 res=llm.with_structured_output(Plan).invoke(planner_prompt(prompt))
 
 print(res)
+
+graph=StateGraph(dict)
+
+graph.add_node("planner",planner_agent)
+
+graph.set_entry_point("planner")
+
+agent=graph.compile()
+
+
+
+result=agent.invoke({"user_prompt": user_prompt})
+print(result)
